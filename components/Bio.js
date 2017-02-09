2c0c5ac2f8b71c68a91faf8fe2b292359acc3f17
Paolo Bueno <paolohaji@gmail.com>
2017-02-09 00:20:13 -0200

    Gatsby init

---
+++ components/Bio.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { config } from 'config'
+import { rhythm } from 'utils/typography'
+
+class Bio extends React.Component {
+  render () {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        Written by <strong>{config.authorName}</strong>, a Brazilian based in São Paulo, working as a software engineer at <a href="https://www.redhat.com">Red Hat</a>.
+      </p>
+    )
+  }
+}
+
+export default Bio
